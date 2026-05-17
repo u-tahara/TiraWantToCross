b5f33e925e44e08bedd59c84190fe43363c10f67
u-tahara <1029rie.u.chan@gmail.com>
2026-05-18 01:46:03 +0900

    Landscape回転時のCanvasスケール破綻を修正

--- Assets/Scripts/PrototypeStageUiController.cs
+++ Assets/Scripts/PrototypeStageUiController.cs
@@ -129,7 +129,7 @@ namespace TiraWantToCross.Prototype
             var scaler = canvasGo.GetComponent<CanvasScaler>();
             scaler.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
             scaler.referenceResolution = new Vector2(1080, 1920);
-            scaler.matchWidthOrHeight = 0.5f;
+            scaler.matchWidthOrHeight = 1.0f;
 
             if (FindAnyObjectByType<EventSystem>() == null)
             {
